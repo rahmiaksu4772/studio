dfde196595faee28359d781d0b73a7c8217edb87
Rahmi Aksu <rahmi.aksu.47@gmail.com>
2025-07-30 11:55:07 +0000

    SOL MENÜ ALIR KAPANIR OLSUN

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -105,7 +105,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
       <div className="flex flex-col sm:pl-[var(--sidebar-width-icon)] group-data-[state=expanded]:sm:pl-[var(--sidebar-width)] transition-[padding-left] duration-200">
          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            <SidebarTrigger className="sm:hidden" />
+            <SidebarTrigger />
         </header>
         {children}
       </div>
