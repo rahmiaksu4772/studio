ffd65ac4d96b385264052adf0857cc3857812d20
Rıdvan ÖZKAN <ridvan.ozkan.183@gmail.com>
2025-09-05 18:15:11 +0000

    Adım 1: Firebase Konfigürasyonunu Kontrol Edin firebase.js veya firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
 
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 import { adminDb } from "../../src/lib/firebase-admin"; // Merkezi SDK örneğini kullan
+import { getMessaging } from "firebase-admin/messaging";
 
-// SDK zaten merkezi olarak başlatıldığı için burada tekrar başlatmaya gerek yok.
-const messaging = admin.messaging();
+const messaging = getMessaging();
 
 // This function triggers when a new document is created in the /notifications collection.
 export const sendNotificationOnCreate = functions.region('europe-west1').firestore
